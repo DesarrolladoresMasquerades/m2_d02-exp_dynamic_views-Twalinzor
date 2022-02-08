0cbcb6718916c68cb5fd4128c15cfc2b55dc86fd
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-02-08 15:45:18 +0100

    .

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const stanData = require("./stan.json");
+const hbs = require("hbs");
 
 require("dotenv").config();
 
 const app = express();
 
+app.set("view engine", "hbs");
+
+app.set("views", __dirname + "/views");
+
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(express.static("public"));
 
+app.get("/home", (req, res, next) => res.render("home"));
 
-app.get("/home", (req, res, next) =>
-res.sendFile(__dirname + "/views/home.html")
+app.get("/about", (req, res, next) => res.render("about"));
+
+app.get("/", (req, res, next) =>
+  // if you need specific format for the data, prepare it outside of the render and then render it
+  res.render("index", stanData)
 );
-
-app.get("/about", (req, res, next) =>
-res.sendFile(__dirname + "/views/about.html")
-);
-
-app.get("/", (req, res, next) => res.send("Hello there!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
